118da27d64bde4744a87d9c2ddfcf44e5d45315b
Frank.Chen <mikru168@gmail.com>
2018-01-13 19:54:17 +0800

    CollectionViewWithStoryboard

---
+++ CollectionViewWithStoryboard/MyCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MyCollectionViewCell.swift
+//  CollectionViewWithStoryboard
+//
+//  Created by Frank.Chen on 2018/1/13.
+//  Copyright © 2018年 frank.chen. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ CollectionViewWithStoryboard/ViewController.swift
@@ -0,0 +1,106 @@
+//
+//  ViewController.swift
+//  CollectionViewWithStoryboard
+//
+//  Created by Frank.Chen on 2018/1/13.
+//  Copyright © 2018年 frank.chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let nib = UINib(nibName: "MyCollectionViewCell", bundle: nil)
+        self.collectionView.register(nib, forCellWithReuseIdentifier: "MyCollectionViewCell")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+extension ViewController: UICollectionViewDataSource {
+    
+    /// 設定 CollectionViewCell
+    ///
+    /// - Parameters:
+    ///   - collectionView: _
+    ///   - indexPath: _
+    /// - Returns: _
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MyCollectionViewCell", for: indexPath)
+        // 設定背景色
+        cell.backgroundColor = indexPath.row % 2 == 0 ? UIColor.orange : UIColor.brown
+        
+        return cell
+    }
+    
+    /// 設定有幾個 CollectionViewCell 在 CollectionView 裡
+    ///
+    /// - Parameters:
+    ///   - collectionView: _
+    ///   - section: _
+    /// - Returns: _
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+}
+
+
+// MARK: - 設定 CollectionView Cell 與 Cell 之間的間距、距確 Super View 的距離等等
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    
+    /// 設定 Collection View 距離 Super View上、下、左、下間的距離
+    ///
+    /// - Parameters:
+    ///   - collectionView: _
+    ///   - collectionViewLayout: _
+    ///   - section: _
+    /// - Returns: _
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 20, left: 10, bottom: 10, right: 10)
+    }
+    
+    ///  設定 CollectionViewCell 的寬、高
+    ///
+    /// - Parameters:
+    ///   - collectionView: _
+    ///   - collectionViewLayout: _
+    ///   - indexPath: _
+    /// - Returns: _
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: (self.view.frame.size.width - 30) / 2 , height: (self.view.frame.size.width - 30) / 2)
+    }
+    
+    /// 滑動方向為「垂直」的話即「上下」的間距(預設為重直)
+    ///
+    /// - Parameters:
+    ///   - collectionView: _
+    ///   - collectionViewLayout: _
+    ///   - section: _
+    /// - Returns: _
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 30
+    }
+    
+    /// 滑動方向為「垂直」的話即「左右」的間距(預設為重直)
+    ///
+    /// - Parameters:
+    ///   - collectionView: _
+    ///   - collectionViewLayout: _
+    ///   - section: _
+    /// - Returns: _
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
+}
