51cb0bf10376402e937e98a02e2b639125f1f343
Josh Lee <joshlee>
2019-10-18 15:37:55 +0800

    Layout setup
    Scroll Direction added

--- CollectionViewWithStoryboard/ViewController.swift
+++ CollectionViewWithStoryboard/ViewController.swift
@@ -10,21 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var myCollView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        let myCollViewLayout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        //myCollViewLayout.sectionInset = UIEdgeInsets(top: 20, left: 10, bottom: 10, right: 10) // section與section之間的距離
+        //myCollViewLayout.itemSize = CGSize(width: (self.view.frame.size.width - 30) / 2, height: 120) // cell的寬、高
+        //myCollViewLayout.minimumLineSpacing = CGFloat(integerLiteral: 10) // collectionView設定為縱向的話即「行」的間距、橫向則為「列」的間距
+        //myCollViewLayout.minimumInteritemSpacing = CGFloat(integerLiteral: 10) // collectionView設定為縱向的話即「列」的間距、橫向則為「行」的間距
+        myCollViewLayout.scrollDirection = UICollectionViewScrollDirection.horizontal // 滑動方向，預設為垂直。注意若設為垂直，則cell的加入方式為由左至右，滿了才會換行；若是水平則由上往下，滿了才會換列
+    
         let nib = UINib(nibName: "MyCollectionViewCell", bundle: nil)
-        self.collectionView.register(nib, forCellWithReuseIdentifier: "MyCollectionViewCell")
+        self.myCollView.register(nib, forCellWithReuseIdentifier: "MyCollectionViewCell")
+        self.myCollView.setCollectionViewLayout(myCollViewLayout, animated: true)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
@@ -72,7 +79,7 @@ extension ViewController: UICollectionViewDelegateFlowLayout {
     ///   - section: _
     /// - Returns: _
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 20, left: 10, bottom: 10, right: 10)
+        return UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
     }
     
     ///  設定 CollectionViewCell 的寬、高
@@ -83,10 +90,10 @@ extension ViewController: UICollectionViewDelegateFlowLayout {
     ///   - indexPath: _
     /// - Returns: _
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: (self.view.frame.size.width - 30) / 2 , height: (self.view.frame.size.width - 30) / 2)
+        return CGSize(width: (self.view.frame.size.width - 30) / 2 , height: (self.view.frame.size.height - 30) / 4)
     }
     
-    /// 滑動方向為「垂直」的話即「上下」的間距(預設為重直)
+    /// 滑動方向為「垂直」的話即「上下」的間距(預設為垂直)
     ///
     /// - Parameters:
     ///   - collectionView: _
@@ -94,10 +101,10 @@ extension ViewController: UICollectionViewDelegateFlowLayout {
     ///   - section: _
     /// - Returns: _
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 30
+        return 10
     }
     
-    /// 滑動方向為「垂直」的話即「左右」的間距(預設為重直)
+    /// 滑動方向為「垂直」的話即「左右」的間距(預設為垂直)
     ///
     /// - Parameters:
     ///   - collectionView: _
